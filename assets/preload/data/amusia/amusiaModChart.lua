b3d2cd41c860ed6fa5a52c6f65f316692c2e9697
Mateuzinholol <116360372+Mateuswuz@users.noreply.github.com>
2022-12-28 13:33:48 -0300

    Add files via upload

---
+++ assets/preload/data/amusia/amusiaModChart.lua
@@ -0,0 +1,39 @@
+local defaultNotePos = {};
+local arrowMoveX = 0;
+local arrowMoveY = -14;
+
+function onStepHit()
+if curStep == 810 then
+    for i = 0,7 do 
+        x = getPropertyFromGroup('strumLineNotes', i, 'x')
+        y = getPropertyFromGroup('strumLineNotes', i, 'y')
+        table.insert(defaultNotePos, {x,y})
+    end
+end
+if curStep == 1312 then
+arrowMoveX = 40
+end
+if curStep == 1456 then
+arrowMoveX = 60
+end
+if curStep == 1472 then
+arrowMoveX = 40
+end
+if curStep == 1488 then
+arrowMoveX = 60
+end
+if curStep == 1504 then
+arrowMoveX = 40
+end
+end
+
+function onUpdate(elapsed)  
+if curStep > 812 then
+    songPos = getPropertyFromClass('Conductor', 'songPosition');
+    currentBeat = (songPos / 1000) * (bpm / 120 )     
+         for i = 0,7 do 
+            setPropertyFromGroup('strumLineNotes', i, 'x', defaultNotePos[i + 1][1] + arrowMoveX * math.sin((currentBeat + i*0.25) * math.pi))
+            setPropertyFromGroup('strumLineNotes', i, 'y', defaultNotePos[i + 1][2] + arrowMoveY * math.cos((currentBeat + i*0.25) * math.pi))
+        end
+end
+end
